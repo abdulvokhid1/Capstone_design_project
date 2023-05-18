31abe6b5bd9d2eedf73c9f622bc2fbe7db67a464
abdulvokhid <73060118+abdulvokhid1@users.noreply.github.com>
2023-05-18 13:33:43 +0900

    added

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,7 @@
+/** @format */
+import React from "react";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+export default Navbar;
---
+++ src/root/index.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Root = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+export default Root;
