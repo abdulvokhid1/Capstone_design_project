184b02378a24aa54b5e5d0b9a6dd3c8c8b03dff4
abdulvokhid <73060118+abdulvokhid1@users.noreply.github.com>
2023-05-20 13:15:41 +0900

    added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,28 @@
 /** @format */
 import React from "react";
-import { Wrapper } from "./styled";
+import { Wrapper, Left, Logo, Center, PageWrapper, Link } from "./styled";
+import logo from "../../assets/icons/logo.svg";
+import { data } from "../../utils/navbar";
 
 const Navbar = () => {
-  return <Wrapper>Navbar</Wrapper>;
+  return (
+    <Wrapper>
+      <Left>
+        <Logo src={logo} />
+      </Left>
+      <Center>
+        {data.map(({ title, pathname, id }) => {
+          return (
+            <PageWrapper>
+              <Link>
+                to-{pathname}
+                {title}
+              </Link>
+            </PageWrapper>
+          );
+        })}
+      </Center>
+    </Wrapper>
+  );
 };
 export default Navbar;
