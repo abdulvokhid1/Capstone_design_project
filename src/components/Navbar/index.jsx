9c1a292454a8ed26cdb5a8ded2d6d0fe80d43b14
abdulvokhid <73060118+abdulvokhid1@users.noreply.github.com>
2023-05-22 13:05:21 +0900

    newfeaturesadded

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,17 @@
 /** @format */
-import React from "react";
+import React, { useState, useContext } from "react";
 import { Wrapper, Left, Logo, Center, PageWrapper, Link } from "./styled";
 import logo from "../../assets/icons/logo.svg";
 import { data } from "../../utils/navbar";
-
+import ProductContext from "../../context/Products";
+import { AuthorizationData } from "../../context/Authrization";
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
+  const [activeLogin, setActiveLogin] = useState("login");
+  const [authedData, setAutheddata] = useContext(AuthorizationData);
+  const [, setNavigateTo] = useContext(Navigator);
+  const [productData] = useContext(ProductContext);
+  const navigate = useNavigate();
   return (
     <Wrapper>
       <Left>
--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import { Router as BrowserRouter, Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Root from "./root/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Root />
-    </Router>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Root />
+  </BrowserRouter>
 );
 
 reportWebVitals();
