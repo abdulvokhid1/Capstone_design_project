59bbc5e7b9f5d07a3a6348df1982ef080c51d633
abdulvokhid <73060118+abdulvokhid1@users.noreply.github.com>
2023-05-23 14:45:11 +0900

    added new features

---
+++ src/components/Navbar/Register/index.jsx
@@ -0,0 +1,8 @@
+/** @format */
+
+import React from "react";
+
+const Register = () => {
+  return <div>Register</div>;
+};
+export default Register;
---
+++ src/utils/auth/data.js
@@ -0,0 +1,13 @@
+/** @format */
+
+export const data = [
+  {
+    role: "admin",
+    password: "admin",
+    email: "admin@gmail.com",
+    user: "admin",
+    firstName: "admin",
+    lastName: "Adminovich",
+    phone: +998995321025,
+  },
+];
