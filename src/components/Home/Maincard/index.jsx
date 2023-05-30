20acfa7ed562affffc8cef2cb3148c6a87268016
Abdulvokhid Ibrakhimov <abdulvokhidibrakhimov@Abdulvokhidui-MacBookPro.local>
2023-05-30 13:19:54 +0900

    added

---
+++ src/components/Home/Maincard/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ const MainCard = () => {
+  return (
+    <div>MainCard</div>
+  )
+}
+export default MainCard
---
+++ src/components/Home/MapData/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ const MapData = () => {
+  return (
+    <div>MapData</div>
+  )
+}
+ export default MapData
