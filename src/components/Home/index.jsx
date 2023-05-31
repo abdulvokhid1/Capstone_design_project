229b0a51665f12996e12d94aef5e25ee6bd1437b
Abdulvokhid Ibrakhimov <ibrakhimov7747a@gmail.com>
2023-05-31 14:18:57 +0900

    done

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,22 @@
-/** @format */
-
-import React from "react";
+import React from 'react';
+import Categories from './Categories Dashboard';
+import CreateNewsletter from './CreateNewsletter';
+import MainCard from './MainCard';
+import MapData from './MapData';
+import { Container } from './style';
+// import { data } from '../../utils/auth/data';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <MainCard />
+      <Container>
+        <Categories />
+        <MapData />
+      </Container>
+      <CreateNewsletter />
+    </div>
+  );
 };
 
 export default Home;
